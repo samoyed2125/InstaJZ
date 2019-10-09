3245db1297016aead06bb83c3e5a69380a0d735f
xuqizhi <xuqizhi@Qizhi.T-mobile.com>
2019-10-08 21:20:49 -0400

    Final prep for heroku

--- Insta/settings.py
+++ Insta/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'posts'
 
